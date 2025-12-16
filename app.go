c08153407f289586bd0ce513a11b445071f2cb15
King Vagandar <poohzazazaja@gmail.com>
2025-12-16 22:39:58 +0700

    Clarify comment for template parsing in app.go

--- app.go
+++ app.go
@@ -11,7 +11,7 @@ import (
 //go:embed templates/*
 var resources embed.FS
 
-// Parse templates
+// Parse templates from the embedded filesystem
 var t = template.Must(template.ParseFS(resources, "templates/*"))
 
 func main() {
